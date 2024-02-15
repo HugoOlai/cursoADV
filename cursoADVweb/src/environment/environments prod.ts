501e330bb1912681c4f3ddc52045e5fdd67f97f4
Hugo Olaitan <holaitan@proadv.adv.br>
2024-02-15 00:56:13 -0300

    adicionando url

---
+++ cursoADVweb/src/environment/environments prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  baseUrl : 'http://centroproinf.com.br/api',
+  checkoutUrl : 'http://centroproinf.com.br',
+  key : '202dacd275cec7cd77cbd1923e16b5d0',
+  access : '@@71XX',
+  // appVersion: require('../../package.json').version + '-dev',
+};
---
+++ cursoADVweb/src/environment/environments.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  baseUrl : 'https://localhost:44359/api',
+  key : '202dacd275cec7cd77cbd1923e16b5d0',
+  access : '@@71XX',
+  // appVersion: require('../../package.json').version + '-dev',
+};

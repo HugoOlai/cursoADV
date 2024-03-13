36d8cebba256efa65bcbdec89b42bda8b6310ffc
Hugo Olaitan <holaitan@proadv.adv.br>
2024-03-13 08:15:30 -0300

    adicionando menu vertical

--- cursoADVweb/src/app/blog/blog.module.ts
+++ cursoADVweb/src/app/blog/blog.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BlogComponent } from './blog.component';
 import { BlogRoutes } from './blog.routing.module';
+import { CarouselModule } from '../components/carousel/carousel.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    BlogRoutes
+    BlogRoutes,
   ],
   declarations: [BlogComponent]
 })

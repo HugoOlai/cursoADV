82581329290ac39514c2891e0542b48e14afc874
Hugo Olaitan <holaitan@proadv.adv.br>
2024-03-13 08:15:46 -0300

    removendo importação

--- cursoADVweb/src/app/blog/blog.module.ts
+++ cursoADVweb/src/app/blog/blog.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BlogComponent } from './blog.component';
 import { BlogRoutes } from './blog.routing.module';
-import { CarouselModule } from '../components/carousel/carousel.module';
 
 @NgModule({
   imports: [

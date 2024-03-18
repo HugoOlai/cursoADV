bf7db661ec27b93ca6763b5e92e3c13b5ff30133
Hugo Olaitan <holaitan@proadv.adv.br>
2024-03-17 22:26:32 -0300

    Implementando paginaInicial (as portas de finalizar)

--- cursoADVweb/src/app/blog/pages/inicio/inicio.component.ts
+++ cursoADVweb/src/app/blog/pages/inicio/inicio.component.ts
@@ -8,9 +8,29 @@ import { Util } from '../../../class/util.class';
 })
 export class InicioComponent implements OnInit {
 
-  ListaBanners: any = [
+  vira = false;
+  vira2 = false;
+  vira3 = false;
+  texto: string = 'Cursos elaborados por especialistas para transformá-lo em um profissional altamente qualificado para concursos de Defensoria Pública.';
+  texto2: string = 'Técnicas de ensino avançadas que promovem uma aprendizagem profunda e prática, preparando você para os desafios reais dos concursos e da carreira jurídica.';
+  texto3: string = ' Acompanhamento em grupo e acesso a uma rede de estudantes e profissionais engajados, fortalecendo sua preparação e motivação.';
+
+  sumiritem = false;
+
+  ListaAvaliacoes: Array<any> = [
+    {nome: 'Hugo', css:'pequeno'},
+    {nome: 'Vanessa', css:'grande'},
+    {nome: 'Bruno', css:'pequeno'},
+  ]
+
+  ListaBannersCursos: any = [
     {src: '../../../../assets/imgs/capaCursosEcaProInf.png'},
     {src: '../../../../assets/imgs/bannerrotativo1.png'},
+  ]
+
+  ListaBanners: any = [
+    {src: '../../../../assets/imgs/bannerrotativo1.png'},
+    {src: '../../../../assets/imgs/capaCursosEcaProInf.png'},
   ]
 
   caroucelBtns: string = '';
@@ -27,6 +47,90 @@ export class InicioComponent implements OnInit {
       <img src="${banners.src}" class="d-block w-100" alt="...">
     </div>`
     });
+  }
+
+  ngOnChanges(changes: import("@angular/core").SimpleChanges): void {
+    console.log(changes)
+  }
+
+  virar(tipo: number){
+    console.log('passeo aqui')
+    switch(tipo){
+      case 1:
+        this.vira = true;
+      break;
+      case 2:
+      this.vira2 = true;
+
+      break;
+      default:
+        this.vira3 = true;
+    }
+  }
+
+  desVirar(tipo: number){
+    console.log('desVirar')
+    console.log(this.sumiritem)
+    // this.vira = false;
+    switch(tipo){
+      case 1:
+        this.vira = false;
+      break;
+      case 2:
+      this.vira2 = false;
+
+      break;
+      default:
+      this.vira3 = false;
+    }
+
+  }
+
+  direita(){
+    console.log(this.ListaAvaliacoes)
+    var lista:any = [];
+    var cont = 0;
+    var principal = false;
+    this.ListaAvaliacoes.forEach((element:any) => {
+      if(cont > 0){
+        if(cont == 1){
+          element.css = 'pequeno'
+          lista.push(element);
+
+        }else{
+          if(!principal){
+            element.css = 'grande'
+            lista.push(element);
+            principal = true;
+          } else {
+            lista.push(element);
+          }
+        }
+      }
+      cont++
+    });
+
+    lista.push(this.ListaAvaliacoes[0])
+    this.ListaAvaliacoes = lista;
+    console.log(this.ListaAvaliacoes)
+
+  }
+
+  esquerda(){
+    console.log(this.ListaAvaliacoes)
+    var lista:any = [];
+    var cont = 0
+    this.ListaAvaliacoes.reverse().forEach((element:any) => {
+      if(cont > 0){
+        lista.push(element);
+      }
+      cont++
+    });
+
+    lista.push(this.ListaAvaliacoes[0])
+    this.ListaAvaliacoes = lista;
+    console.log(this.ListaAvaliacoes)
+
   }
 
 }
